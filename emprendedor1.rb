7e5175a9adfbb0b7176cbaac417cce809cea4343
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-10 22:51:54 -0300

    perfeccionando el codigo

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -1,3 +1,12 @@
+=begin
+El producto planea venderse en 50 dólares
+Se espera tener 1000 usuarios en el año
+Los gastos del año son 20000 dólares
+Las utilidades se calculan como
+Los impuestos aplicados a las utilidades 
+son el 35% y solo aplican si es positivo.
+=end
+
 precio=ARGV[0].to_i 
 usuarios=ARGV[1].to_i
 gastos=ARGV[2].to_i
@@ -8,6 +17,6 @@ utilidades_con_impuesto=(precio*usuarios-gastos)*35/100
 resultado=(utilidades-utilidades_con_impuesto)
 
 if utilidades >= 0
-    puts "Utilidades #{resultado} con impuesto aplicado"
+    puts "Las utilidades son: $#{resultado} con impuesto aplicado"
 end
 
